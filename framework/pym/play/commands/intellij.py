2fef0aaafd075b9af6c62980b83bd95e2191df54
Andrei Fokau <andrei.fokau@neutron.kth.se>
2011-07-16 00:50:03 +0200

    Fix the workspace template to expand project tree

--- framework/pym/play/commands/intellij.py
+++ framework/pym/play/commands/intellij.py
@@ -28,6 +28,7 @@ def execute(**kargs):
         iwsFile = os.path.join(app.path, application_name + '.iws') # workspace file
     
         shutil.copyfile(os.path.join(play_env["basedir"], 'resources/idea/iwsTemplate.xml'), iwsFile)
+        replaceAll(iwsFile, r'%APP_DIR%', os.path.basename(app.path))
         replaceAll(iwsFile, r'%APP_NAME%', application_name)
         
         shutil.copyfile(os.path.join(play_env["basedir"], 'resources/idea/iprTemplate.xml'), iprFile)
